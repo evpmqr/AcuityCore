b6c73c945061cf0b9012ba261b0e8c2411261cf8
Eclipseop <guest173800@gmail.com>
2017-06-02 11:53:21 -0400

    adding pricechecker

---
+++ http-api/src/main/java/com/acuity/http/api/PriceLookup.java
@@ -0,0 +1,53 @@
+package com.acuity.http.api;
+
+/**
+ * Created by Eclipseop.
+ * Date: 6/1/2017.
+ */
+public class PriceLookup {
+
+	private long overall;
+	private long buying;
+	private long buyingQuantity;
+	private long selling;
+	private long sellingQuantity;
+
+	protected PriceLookup() {
+		overall = -1;
+		buying = -1;
+		buyingQuantity = -1;
+		selling = -1;
+		sellingQuantity = -1;
+	}
+
+	public long getBuying() {
+		return buying;
+	}
+
+	public long getBuyingQuantity() {
+		return buyingQuantity;
+	}
+
+	public long getSellingQuantity() {
+		return sellingQuantity;
+	}
+
+	public long getOverall() {
+		return overall;
+	}
+
+	public long getSelling() {
+		return selling;
+	}
+
+	@Override
+	public String toString() {
+		return "PriceLookup{" +
+				"overall=" + overall +
+				", buying=" + buying +
+				", buyingQuantity=" + buyingQuantity +
+				", selling=" + selling +
+				", sellingQuantity=" + sellingQuantity +
+				'}';
+	}
+}
