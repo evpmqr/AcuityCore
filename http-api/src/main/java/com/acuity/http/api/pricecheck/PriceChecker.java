acb8e907f4eb1c55166005cf7edf1b8ff5e359fe
Zachary Herridge <zgherridge@gmail.com>
2017-06-06 15:28:14 -0500

    Changed PriceChecker structure to use Google Cache more effectively.

--- http-api/src/main/java/com/acuity/http/api/pricecheck/PriceChecker.java
+++ http-api/src/main/java/com/acuity/http/api/pricecheck/PriceChecker.java
@@ -3,11 +3,13 @@ package com.acuity.http.api.pricecheck;
 import com.acuity.http.api.AcuityWebAPI;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -21,12 +23,16 @@ public class PriceChecker {
 			.expireAfterWrite(10, TimeUnit.MINUTES)
 			.build();
 
-	public static PriceLookup getLookup(final int itemId) {
-		PriceLookup pl;
-		if ((pl = cache.getIfPresent(itemId)) != null) {
-			return pl;
+	public static PriceLookup find(final int itemId){
+		try {
+			return cache.get(itemId, () -> lookup(itemId));
+		} catch (ExecutionException e) {
+			e.printStackTrace();
 		}
+		return null;
+	}
 
+	private static PriceLookup lookup(final int itemId) {
 		try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new URL(RSB_URL + itemId).openStream()))) {
 			final PriceLookup priceLookup = AcuityWebAPI.INSTANCE.getGson().fromJson(bufferedReader.readLine(), PriceLookup.class);
 			cache.put(itemId, priceLookup);
