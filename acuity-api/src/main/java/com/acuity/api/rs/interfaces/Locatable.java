676184b81f856ca583b0444d81f713a4410cd43a
Zachary Herridge <zgherridge@gmail.com>
2017-06-09 11:21:45 -0500

    Readded local player.

--- acuity-api/src/main/java/com/acuity/api/rs/interfaces/Locatable.java
+++ acuity-api/src/main/java/com/acuity/api/rs/interfaces/Locatable.java
@@ -3,6 +3,7 @@ package com.acuity.api.rs.interfaces;
 import com.acuity.api.RSInstance;
 import com.acuity.api.rs.movement.SceneTile;
 import com.acuity.api.rs.movement.Tile;
+import com.acuity.api.rs.utils.LocalPlayer;
 
 /**
  * Created by Eclipseop.
@@ -10,70 +11,64 @@ import com.acuity.api.rs.movement.Tile;
  */
 public interface Locatable {
 
-	default int getSceneX() {
-		System.out.println("Locatable: returning default sceneX: " + this.getClass().getName());
-		return 0;
-	}
+    default int getSceneX() {
+        System.out.println("Locatable: returning default sceneX: " + this.getClass().getName());
+        return 0;
+    }
 
-	default int getSceneY() {
-		System.out.println("Locatable: returning default sceneY: " + this.getClass().getName());
-		return 0;
-	}
+    default int getSceneY() {
+        System.out.println("Locatable: returning default sceneY: " + this.getClass().getName());
+        return 0;
+    }
 
-	default int getPlane() {
-		return RSInstance.getClient().getPlane();
-	}
+    default int getPlane() {
+        return RSInstance.getClient().getPlane();
+    }
 
-	default int getX() {
-		return toLocation().getX();
-	}
+    default int getX() {
+        return toLocation().getX();
+    }
 
-	default int getY() {
-		return toLocation().getY();
-	}
+    default int getY() {
+        return toLocation().getY();
+    }
 
-	default Tile toLocation() {
-		return new Tile(getSceneX() + RSInstance.getClient().getBaseSceneX(), getSceneY() + RSInstance.getClient().getBaseSceneY(), getPlane());
-	}
+    default Tile toLocation() {
+        return new Tile(getSceneX() + RSInstance.getClient().getBaseSceneX(), getSceneY() + RSInstance.getClient().getBaseSceneY(), getPlane());
+    }
 
-	default SceneTile toSceneTile() {
-		return new SceneTile(getSceneX(), getSceneY(), getPlane());
-	}
+    default SceneTile toSceneTile() {
+        return new SceneTile(getSceneX(), getSceneY(), getPlane());
+    }
 
-	// TODO: 6/8/2017 when players are added
+    default int distance() {
+        return distance(LocalPlayer.getLocation());
+    }
 
-	default int distance() {
-		return -1;
-		//return distance(LocalPlayer.getLocation());
-	}
+    default boolean isOnMiniMap() {
+        return toLocation().isOnMiniMap();
+    }
 
+    default int distance(Locatable locatable) {
+        return (int) Math.round(distancePrecise(locatable));
+    }
 
-	default boolean isOnMiniMap() {
-		return toLocation().isOnMiniMap();
-	}
+    default double distancePrecise() {
+        return distancePrecise(LocalPlayer.getLocation());
+    }
 
-	default int distance(Locatable locatable) {
-		return (int) Math.round(distancePrecise(locatable));
-	}
+    default double distancePrecise(Locatable locatable) {
+        if (locatable == null) {
+            return Integer.MAX_VALUE - 1;
+        }
 
-	/*
-	default double distancePrecise() {
-		return distancePrecise(LocalPlayer.getLocation());
-	}
-	*/
+        Tile t = locatable.toLocation();
+        Tile t2 = toLocation();
 
-	default double distancePrecise(Locatable locatable) {
-		if (locatable == null) {
-			return Integer.MAX_VALUE - 1;
-		}
+        if (this.getPlane() != this.getPlane()) {
+            return Integer.MAX_VALUE - 1;
+        }
 
-		Tile t = locatable.toLocation();
-		Tile t2 = toLocation();
-
-		if (this.getPlane() != this.getPlane()) {
-			return Integer.MAX_VALUE - 1;
-		}
-
-		return Math.hypot(t.getX() - t2.getX(), t.getY() - t2.getY());
-	}
+        return Math.hypot(t.getX() - t2.getX(), t.getY() - t2.getY());
+    }
 }
