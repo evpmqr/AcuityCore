27998785af4007cac4363edd4a738eb0d20f98be
Zachary Herridge <zgherridge@gmail.com>
2017-06-12 11:03:14 -0500

    Cleaned up Varps

--- acuity-api/src/main/java/com/acuity/api/rs/utils/Varps.java
+++ acuity-api/src/main/java/com/acuity/api/rs/utils/Varps.java
@@ -2,6 +2,8 @@ package com.acuity.api.rs.utils;
 
 import com.acuity.api.AcuityInstance;
 import com.google.common.base.Preconditions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Optional;
 
@@ -10,6 +12,8 @@ import java.util.Optional;
  * Date: 6/12/2017.
  */
 public class Varps {
+
+	private static final Logger logger = LoggerFactory.getLogger(Varps.class);
 
 	public static final int MAX_VARP = 2000;
 	public static final int[] BIT_MASKS = new int[32];
@@ -23,22 +27,17 @@ public class Varps {
 	}
 
 	public static Optional<int[]> getAll() {
-		return Optional.ofNullable(AcuityInstance.getClient().getVarps());
+		Optional<int[]> varps = Optional.ofNullable(AcuityInstance.getClient().getVarps());
+		if (varps.map(ints -> ints.length).orElse(0) == 0) return Optional.empty();
+		return varps;
 	}
 
-	public static int get(final int index) {
+	public static int get(final int index, final int defualt) {
 		Preconditions.checkArgument(index < MAX_VARP && index > 0, "Bad index, min index: 1, max index: 1999");
-		final Optional<int[]> optional = getAll();
-
-		if (optional.isPresent()) {
-			final int[] varps = optional.get();
-
-			return varps.length == 0 ? -1 : varps[index];
-		}
-		return -1;
+		return getAll().map(ints -> ints[index]).orElse(defualt);
 	}
 
 	public static boolean getBoolean(final int index) {
-		return get(index) == 1;
+		return get(index, 0) == 1;
 	}
 }
