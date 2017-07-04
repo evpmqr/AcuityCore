70f6ea020dcdf49955f6e1b9e246e1347fca0b3d
Zach <zgherridge@gmail.com>
2017-07-04 08:48:26 -0500

    Injected a synthetic method to load a varpbit and generated the invoker.

--- acuity-api/src/main/java/com/acuity/rs/api/RSClient.java
+++ acuity-api/src/main/java/com/acuity/rs/api/RSClient.java
@@ -407,6 +407,8 @@ public interface RSClient extends RSGameEngine {
 
 	void invokeInsertMenuItem(java.lang.String var0, java.lang.String var1, int var2, int var3, int var4, int var5, boolean var6);
 
+	com.acuity.rs.api.RSVarpbit invokeLoadVarpbit(int var0);
+
 	boolean invokeLoadWorlds();
 
 	void invokeOnSceneXTEAKeyChange(boolean var0);
