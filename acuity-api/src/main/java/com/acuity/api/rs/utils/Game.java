a0d005fd92f037c02410d2e2cc013ded99c4a30f
Eclipseop <guest173800@gmail.com>
2017-07-25 18:09:25 -0400

    More gamestates

--- acuity-api/src/main/java/com/acuity/api/rs/utils/Game.java
+++ acuity-api/src/main/java/com/acuity/api/rs/utils/Game.java
@@ -13,6 +13,8 @@ public class Game {
     public static final int LOGIN_SCREEN = 10;
     public static final int LOADING_SCREEN = 25;
     public static final int IN_GAME = 30;
+    public static final int CONNECTION_LOST = 40;
+    public static final int CONNECTION_RECONNECTING = 45; //maybe
 
     public static int getGameState(){
         return AcuityInstance.getClient().getGameState();
