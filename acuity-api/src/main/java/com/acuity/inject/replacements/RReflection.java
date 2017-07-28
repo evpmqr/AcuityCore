06f48c0b8bf3f5531f3b740656c7b76dac5ed17d
Zachary Herridge <zgherridge@gmail.com>
2017-07-28 11:49:26 -0500

    Rewrote the Jagex Reflection replacement code.

--- acuity-api/src/main/java/com/acuity/inject/replacements/RReflection.java
+++ acuity-api/src/main/java/com/acuity/inject/replacements/RReflection.java
@@ -17,64 +17,74 @@ public class RReflection {
     private static Logger logger = LoggerFactory.getLogger(RReflection.class);
 
     @ClientInvoked
-    public static Class<?> findClass(String className) throws ClassNotFoundException {
-        logger.debug("RS-Reflection finding class '{}'.", className);
-        return Class.forName(className);//http://i.imgur.com/sO0wCM1.png
-    }
-
-    @ClientInvoked
     public static String getMethodName(Method method){
         logger.debug("RS-Reflection getting name of method {}.", method);
         return method.getName();
     }
 
     @ClientInvoked
-    public static Class[] getParameterTypes(Method method){
+    public static Class[] getMethodParameterTypes(Method method){
         logger.debug("RS-Reflection finding method parameters {}.", method);
         return method.getParameterTypes();
     }
 
     @ClientInvoked
-    public static Field findField(Class clazz, String fieldName) throws NoSuchFieldException {
-        logger.debug("RS-Reflection finding field '{}' in {}.", fieldName, clazz);
-        return clazz.getDeclaredField(fieldName);
-    }
-
-    @ClientInvoked
-    public static Object invoke(Method method, Object instance, Object[] params) throws InvocationTargetException, IllegalAccessException {
-        logger.debug("RS-Reflection invoking method {} on instance {}, with params {}.", method, instance, Arrays.toString(params));
-        return method.invoke(instance, params);
-    }
-
-
-    @ClientInvoked
-    public static void setInt(Field field, Object instance, int value) throws IllegalAccessException {
-        logger.debug("RS-Reflection setting field {} instance {} to {}.", field, instance, value);
-        field.setInt(instance, value);
-    }
-
-    @ClientInvoked
-    public static int getModifiers(Field field) {
-        logger.debug("RS-Reflection getting modifiers of field {}.", field);
-        return field.getModifiers();
-    }
-
-    @ClientInvoked
-    public static Class getReturnType(Method method) {
+    public static Class getMethodReturnType(Method method) {
         logger.debug("RS-Reflection getting return-type of method {}.", method);
         return method.getReturnType();
     }
 
-
     @ClientInvoked
-    public static int getModifiers(Method method) {
+    public static int getMethodModifiers(Method method) {
         logger.debug("RS-Reflection getting modifiers of method {}.", method);
         return method.getModifiers();
     }
 
     @ClientInvoked
-    public static int getInt(Field field, Object instance) throws IllegalAccessException {
+    public static Object invokeMethod(Method method, Object instance, Object[] params) throws InvocationTargetException, IllegalAccessException {
+        logger.debug("RS-Reflection invoking method {} on instance {}, with params {}.", method, instance, Arrays.toString(params));
+        return method.invoke(instance, params);
+    }
+
+    @ClientInvoked
+    public static int getFieldInt(Field field, Object instance) throws IllegalAccessException {
         logger.debug("RS-Reflection getting field {} int value instance {}.", field, instance);
         return field.getInt(instance);
+    }
+
+    @ClientInvoked
+    public static void setFieldInt(Field field, Object instance, int value) throws IllegalAccessException {
+        logger.debug("RS-Reflection setting field {} instance {} to {}.", field, instance, value);
+        field.setInt(instance, value);
+    }
+
+    @ClientInvoked
+    public static int getFieldModifiers(Field field) {
+        logger.debug("RS-Reflection getting modifiers of field {}.", field);
+        return field.getModifiers();
+    }
+
+    @ClientInvoked
+    public static ClassLoader getClassLoader(Class clazz){
+        logger.debug("RS-Reflection getting classloader of class {}.", clazz);
+        return clazz.getClassLoader();
+    }
+
+    @ClientInvoked
+    public static Field getDeclaredField(Class clazz, String fieldName) throws NoSuchFieldException {
+        logger.debug("RS-Reflection getting declared field '{}' from {}.", fieldName, clazz);
+        return clazz.getDeclaredField(fieldName);
+    }
+
+    @ClientInvoked
+    public static Method[] getDeclaredMethods(Class clazz){
+        logger.debug("RS-Reflection getting declared methods from {}.", clazz);
+        return clazz.getDeclaredMethods();
+    }
+
+    @ClientInvoked
+    public static Class classForName(String className) throws ClassNotFoundException {
+        logger.debug("RS-Reflection getting class named '{}'.", className);
+        return Class.forName(className);
     }
 }
