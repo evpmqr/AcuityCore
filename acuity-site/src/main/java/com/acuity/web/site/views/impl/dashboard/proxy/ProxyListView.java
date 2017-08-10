aa0b912edb1d2da66f7ecac83f64b7c989a70ece
Zachary Herridge <zgherridge@gmail.com>
2017-08-10 09:31:27 -0500

    Delete proxy.

--- acuity-site/src/main/java/com/acuity/web/site/views/impl/dashboard/proxy/ProxyListView.java
+++ acuity-site/src/main/java/com/acuity/web/site/views/impl/dashboard/proxy/ProxyListView.java
@@ -9,6 +9,7 @@ import com.vaadin.icons.VaadinIcons;
 import com.vaadin.navigator.View;
 import com.vaadin.server.VaadinSession;
 import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
 
 import java.util.List;
 
@@ -39,9 +40,20 @@ public class ProxyListView extends VerticalLayout implements View {
         grid.addColumn(Proxy::getPort).setCaption("Port");
         grid.addColumn(Proxy::getUsername).setCaption("Username").setHidden(true);
         grid.addColumn(proxy -> proxy.getPassword() != null ? Strings.repeat("*", 10) : "").setCaption("Password").setHidden(true);
-        grid.addComponentColumn(client -> {
+        grid.addComponentColumn(proxy -> {
             HorizontalLayout content = new HorizontalLayout();
 
+            Button remove = new Button(VaadinIcons.MINUS_CIRCLE);
+            remove.addStyleName(ValoTheme.BUTTON_ICON_ONLY);
+            remove.addStyleName(ValoTheme.BUTTON_TINY);
+            remove.addStyleName(ValoTheme.BUTTON_DANGER);
+            remove.addClickListener(clickEvent -> {
+                ProxyService.getInstance().getCollection().deleteDocument(proxy.getKey());
+                proxies.removeIf(proxy1 -> proxy1.getKey().equals(proxy.getKey()));
+                grid.getDataProvider().refreshAll();
+            });
+
+            content.addComponent(remove);
 
             return content;
         }).setCaption("Actions").setSortable(false);
@@ -52,6 +64,12 @@ public class ProxyListView extends VerticalLayout implements View {
         addComponent(grid);
     }
 
+    private void refreshData(){
+        proxies.clear();
+        proxies.addAll(ProxyService.getInstance().getByOwner(acuityAccount.getID()));
+        grid.getDataProvider().refreshAll();
+    }
+
     private void buildAddProxy(){
         Button addProxy = new Button("Add", clickEvent -> {
             final Window window = new Window("Add Proxy");
@@ -59,11 +77,7 @@ public class ProxyListView extends VerticalLayout implements View {
             AddProxyForm addProxyForm = new AddProxyForm(window);
             addProxyForm.setMargin(true);
             window.setContent(addProxyForm);
-            window.addCloseListener(closeEvent -> {
-                proxies.clear();
-                proxies.addAll(ProxyService.getInstance().getByOwner(acuityAccount.getID()));
-                grid.getDataProvider().refreshAll();
-            });
+            window.addCloseListener(closeEvent -> refreshData());
             getUI().addWindow(window);
         });
         addProxy.setIcon(VaadinIcons.PLUS_CIRCLE);
